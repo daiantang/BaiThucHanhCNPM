fac7e9b1252028f58dbde68bff5907a81de3de05
daiantang <1851010003an@ou.edu.vn>
2022-10-18 09:35:00 +0700

    init project saleapp

---
+++ FlaskSaleApp/app/templates/index.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+from app import dao
+
+
+@app.route('/')
+def index():
+    categories=dao.load_categories()
+    return render_template('index.html', categories=categories)
+
+if __name__ == '__main__':
+    app.run(debug=True)
